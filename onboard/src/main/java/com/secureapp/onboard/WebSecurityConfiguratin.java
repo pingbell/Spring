fb62ab4ac93f88839e6494c50007dc0f6ea2d951
pingbell <pingbell@outlook.com>
2020-05-11 09:56:25 +0530

    simple spring boot security login

---
+++ onboard/src/main/java/com/secureapp/onboard/WebSecurityConfiguratin.java
@@ -0,0 +1,31 @@
+package com.secureapp.onboard;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@EnableWebSecurity
+public class WebSecurityConfiguratin extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password(passwordEncoder().encode("1234"))
+		.roles("USER")
+		.and()
+		.withUser("foo")
+		.password(passwordEncoder().encode("1234"))
+		.roles("admin");
+		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+}
